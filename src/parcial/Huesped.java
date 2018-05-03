f1d853f592b53b0d6f5523cbfb9247174cb9873b
Margarita <00225117>
2018-05-03 12:51:11 -0600

    se modifico clase Huesped

--- src/parcial/Huesped.java
+++ src/parcial/Huesped.java
@@ -4,11 +4,43 @@
  * and open the template in the editor.
  */
 package parcial;
-
+import java.util.ArrayList;
 /**
  *
  * @author Margarita
  */
 public class Huesped {
+
+    Nombre nombres;
+    int Dui;
+    boolean reserva = false;
+
+    public Nombre getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(Nombre nombres) {
+        this.nombres = nombres;
+    }
+    
+    public boolean getReserva(){
+        return this.reserva;
+    }
+
+    public void setReserva(boolean reserva){
+        this.reserva = reserva;
+    }
+
+    public int getDUI() {
+        return this.Dui;
+    }
+
+    public void setDUI(int Dui) {
+        this.Dui = Dui;
+    }
+    
+    
     
 }
+        
+    
