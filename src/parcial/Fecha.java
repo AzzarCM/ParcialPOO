66c2a826979b3fe316e44a265161c11f82ff0909
Rodolfo Salgado <37157142+AlexanderSalgado@users.noreply.github.com>
2018-05-03 12:27:01 -0600

    Clase fecha

--- src/parcial/Fecha.java
+++ src/parcial/Fecha.java
@@ -11,6 +11,32 @@ package parcial;
  */
 public class Fecha {
     int dia, mes, anio;
+
+    public int getDia() {
+        return this.dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return this.mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAnio() {
+        return this.anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
     
     
     
