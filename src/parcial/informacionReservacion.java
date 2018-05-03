d6543eac5b48b0eb1269372b61ac5d2ead0c99ee
Cristian Mundo <00086917@uca.edu.sv>
2018-05-03 15:39:10 -0600

    correcciones en el metodo modificarHabitacion switch opcion 1

--- src/parcial/informacionReservacion.java
+++ src/parcial/informacionReservacion.java
@@ -27,17 +27,23 @@ public class informacionReservacion {
         
         
         Reservacion lista = new Reservacion();
+        
         infoReserv.add(lista);
         System.out.println("Ingrese el nombre del cliente \n");
         lista.setNombre(read.nextLine());
+        
         System.out.println("Ingrese el numero de DUI del cliente \n");
         lista.setIdHuesped(read.nextLine());
+        
         System.out.println("Ingrese la habitacion del cliente \n");
         lista.setHabitacion(read.nextLine());
+        
         System.out.println("Ingrese la fecha de entrada del cliente por favor: \n");
         lista.setFechaEntrada(read.nextLine());
+        
         System.out.println("Ingrese la fecha de salidad del cliente por favor: \n");
         lista.setFechaSalida(read.nextLine());
+        
         System.out.println("Ingrese el numero de tarjeta de credito del cliente \n");
         lista.setNumeroTarjeta(read.nextLine());
         
@@ -69,37 +75,36 @@ public class informacionReservacion {
     public void modidicarHabitacion(Habitacion habitacion){
         
         int opcion = 4;
-        
         while(opcion!=3){
             
             System.out.println("Que desea hacer? ");
             System.out.println("1- cambiar habitacion");
             System.out.println("2- cambiar fecha");
-            System.out.println("3- salir");
+            System.out.println("3- salir al menu");
             opcion = read.nextInt();
             
             switch (opcion) {
 
                 case 1:
                     String nuevaHabitacion;
-                    
-                    if(infoReserv.get(i).habitacion.getEstado){
-                        
-                        
-                    for (int i = 0; i < infoReserv.size(); i++) {
+          
+                        for (int i = 0; i < infoReserv.size(); i++) {
 
-                        if (infoReserv.get(i).getHabitacion().equals(Habitacion)) {
-                            System.out.println("Ingrese la nueva habitacion para el cliente");
-                            nuevaHabitacion = read.nextLine();
+                            if (infoReserv.get(i).getHabitacion().equals(habitacion) && habitacion.getEstado()) {
+                                System.out.println("Ingrese la nueva habitacion para el cliente");
+                                
+                                nuevaHabitacion = read.nextLine();
 
-                            infoReserv.get(i).setHabitacion(nuevaHabitacion);
+                                infoReserv.get(i).setHabitacion(nuevaHabitacion);
+                                habitacion.setEstado(false);
+                                break;
+                            }
 
                         }
-                        
+                case 2:
+                    
+
                     }
-
-
-        }
         
         
                
@@ -107,8 +112,7 @@ public class informacionReservacion {
             }
             
         }
-        
-    }
+       
     
     
     
