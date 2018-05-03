fb506e865f8872545b914d416d93f45ae38c4f7b
David Vallecios <00078217@uca.edu.sv>
2018-05-02 21:33:10 -0600

    Agregar metodo ModificarPrecio

--- src/parcial/Paquetes.java
+++ src/parcial/Paquetes.java
@@ -5,10 +5,60 @@
  */
 package parcial;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Mi PC
+ * @author rodol
  */
 public class Paquetes {
+    String idPaquete, codeTemporada;
+    float precio;
+    private ArrayList<ArrayList<String>>  paqPrinc;
+    private ArrayList<String>  paq;
+    private ArrayList<String>  paq2;
+    private ArrayList<String>  paq_new;
+    
+    public Paquetes(){
+        paq = new ArrayList<>();
+        paq2 = new ArrayList<>();
+        
+        paqPrinc = new ArrayList<>();
+        paq.add("Basico");
+        paq.add("10");
+        paq2.add("Premium");
+        paq2.add("150");
+        paqPrinc.add(paq);
+        paqPrinc.add(paq2);
+        
+    }
+ 
+    private void agregarPaquete(String nombrePaq, int PrecioPaq){
+        paq_new = new ArrayList<>();
+        paq_new.add(nombrePaq);
+        paq_new.add(Integer.toString(PrecioPaq));
+        paqPrinc.add(paq_new);
+ 
+    }
+    
+    private void modificarPrecio(String nombrePaq, int newPrice){
+        for(int i = 0; i<paqPrinc.size(); i++){
+            ArrayList referen = (ArrayList) paqPrinc.get(i);
+            for(int j = 0; j<referen.size()-1; j++){
+                if(nombrePaq.equals(referen.get(j))){
+                    referen.set(1, newPrice);
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    
+    private void eliminarPrecio(){
+        
+    }
+    
+    
+    
     
 }
