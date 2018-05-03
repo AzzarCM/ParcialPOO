9142e7994d5f762980f6fbac2336b5275b1fe967
Rodolfo Salgado <37157142+AlexanderSalgado@users.noreply.github.com>
2018-05-03 12:51:54 -0600

    Habitacion con su set y get.

---
+++ src/parcial/Habitacion.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial;
+
+/**
+ *
+ * @author rodol
+ */
+public class Habitacion {
+    boolean estado,habilitacion;
+    int numero;
+    String tipo;
+    
+    public Habitacion(boolean estado, int numero){
+        this.estado = estado;
+        this.numero = numero;
+    
+    }
+
+    public boolean getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    
+    
+    
+    
+}
