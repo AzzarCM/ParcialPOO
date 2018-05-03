5f105159341363f8f8ffbc8e6acd4a8fb1ee0a25
Margarita <00225117@uca.edu.sv>
2018-05-03 12:59:58 -0600

    Clase Nombre

---
+++ src/parcial/Nombre.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial;
+
+/**
+ *
+ * @author Margarita
+ */
+public class Nombre {
+    
+    String Nombres, Apellidos;
+    
+    public Nombre(){}
+
+    public String getNombres() {
+        return this.Nombres;
+    }
+
+    public void setNombres(String Nombres) {
+        this.Nombres = Nombres;
+    }
+
+    public String getApellidos() {
+        return this.Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+    
+    
+    
+}
