3f6c61db2f5311cf8d8b8d5d5c4ea03190b934ae
Cristian Mundo <00086917@uca.edu.sv>
2018-05-03 15:37:11 -0600

    llenado del piso con habitaciones

--- src/parcial/Piso.java
+++ src/parcial/Piso.java
@@ -5,12 +5,49 @@
  */
 package parcial;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author cmundo
  */
 public class Piso {
     
+    boolean habilidado = true;
+    ArrayList<Habitacion> habitaciones;
+        // clase piso la cual resibe su nivel.. A B C D E F 
+    
+        public void Piso(String nivel){
+            
+            this.habilidado = true;
+            habitaciones = new ArrayList<>();
+            
+            //Creamos un arrayList de habitaciones y las agregamos 
+            
+            Habitacion hab;
+            hab = new Habitacion(false,1);
+            habitaciones.add(hab);
+            hab = new Habitacion(false,2);
+            habitaciones.add(hab);
+            hab = new Habitacion(false,3);
+            habitaciones.add(hab);
+            hab = new Habitacion(false,4);
+            habitaciones.add(hab);
+            hab = new Habitacion(false,5);
+            habitaciones.add(hab);
+            hab = new Habitacion(false,6);
+            habitaciones.add(hab);
+            hab = new Habitacion(false,7);
+            habitaciones.add(hab);
+            hab = new Habitacion(false,8);
+            habitaciones.add(hab);
+            hab = new Habitacion(false,9);
+            habitaciones.add(hab);
+            hab = new Habitacion(false,10);
+            habitaciones.add(hab);     
+        
+        }
+        
         
     
 }
