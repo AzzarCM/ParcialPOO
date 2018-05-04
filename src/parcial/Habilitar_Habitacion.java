4f91baff88a1e111eee7eccddaeaf1f81526db67
Margarita <00225117@uca.edu.sv>
2018-05-04 01:36:32 -0600

    clase correcta de Habilitar_Habitacion

---
+++ src/parcial/Habilitar_Habitacion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial;
+//import java.util.ArrayList;
+//import java.util.Scanner;
+
+/**
+ *
+ * @author Margarita
+ */
+public class Habilitar_Habitacion {
+     
+   String Piso;
+   int numero;
+   boolean estado;
+   
+
+   
+    public void agregarHabitacion(){
+    
+    }
+    
+
+    public void habilitarHabitacion(String piso, int numero){
+    
+    }
+    public void deshabilitarHabitacion(String piso, int numero){
+    
+    }
+    public void habilitarPiso(String piso){
+    
+    }
+    public void deshabilitarPiso(String piso){
+    
+    }
+    public void liberarReserva(String piso, int numero){
+    
+    }
+}
