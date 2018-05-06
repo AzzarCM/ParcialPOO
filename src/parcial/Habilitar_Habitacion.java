a252c5840deadc94d4e532343c2a462f35dc80f7
David Vallecios <00078217@uca.edu.sv>
2018-05-05 19:14:49 -0600

    Clase Habitacion y sus metodos modificadores

--- src/parcial/Habilitar_Habitacion.java
+++ src/parcial/Habilitar_Habitacion.java
@@ -4,8 +4,8 @@
  * and open the template in the editor.
  */
 package parcial;
-//import java.util.ArrayList;
-//import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -16,15 +16,54 @@ public class Habilitar_Habitacion {
    String Piso;
    int numero;
    boolean estado;
+   private ArrayList<ArrayList<Habitacion>> pisoList;
+   private ArrayList<Habitacion> habList;
+   Scanner lectura = new Scanner(System.in);
    
 
    
-    public void agregarHabitacion(){
-    
+    public void agregarHabitacion(int indice){
+        Habitacion hab = new Habitacion();
+        hab.setEstado(true);
+        System.out.println("Ingrese el numero de la habitacion: ");
+        hab.setNumero(indice);
+        switch(miniMenu()){
+            case 1:
+                hab.setTipo("Sencilla");
+                break;
+            case 2:
+                hab.setTipo("Doble");
+                break;
+            default:
+                System.out.println("Ingrese una opcion valida");
+                break;
+        }
+        habList.add(hab);
     }
     
-
+    public void agregarPiso(){
+        System.out.println("Cuantas habitaciones tendra el nuevo piso: MAXIMO 10");
+        int aux = lectura.nextInt();
+        if(aux > 10){
+            System.out.println("No se puede ingresar mas de 10");
+        }
+        else if(aux <= 0){
+            System.out.println("No se puede ingresar habitaciones con numero menor a 1");
+        }
+        for(int i = 0; i < 10; i++){
+            
+        }
+    }
+ 
+    public int miniMenu(){
+        System.out.println("Ingrese el tipo de habitacion: ");
+        System.out.println("1. Sencilla");
+        System.out.println("2. Doble");
+        return lectura.nextInt();
+    }
+    
     public void habilitarHabitacion(String piso, int numero){
+        
     
     }
     public void deshabilitarHabitacion(String piso, int numero){
