c62519177d06e24b1f7162f6b7a575d40f52d7a6
David Vallecios <00078217@uca.edu.sv>
2018-05-05 19:15:06 -0600

    Clase Habitacion y sus metodos get y set

--- src/parcial/Habitacion.java
+++ src/parcial/Habitacion.java
@@ -13,24 +13,21 @@ public class Habitacion {
     boolean estado,habilitacion;
     int numero;
     String tipo;
-    
-    public Habitacion(boolean estado, int numero){
-        this.estado = estado;
-        this.numero = numero;
-        
-        //verificamos el tipo de habitacion que es DOBLE O SENCILLA
-        
-        this.tipo = (numero%2)==0 ? "Doble":"Sencilla";
-        
-    
-    }
-
+   
     public boolean getEstado() {
         return this.estado;
     }
 
     public void setEstado(boolean estado) {
         this.estado = estado;
+    }
+
+    public boolean getHabilitacion() {
+        return this.habilitacion;
+    }
+
+    public void setHabilitacion(boolean habilitacion) {
+        this.habilitacion = habilitacion;
     }
 
     public int getNumero() {
@@ -42,7 +39,7 @@ public class Habitacion {
     }
 
     public String getTipo() {
-        return tipo;
+        return this.tipo;
     }
 
     public void setTipo(String tipo) {
