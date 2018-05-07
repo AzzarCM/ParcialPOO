0d01bf758e1d30cbcad632db9814069bba1c4606
David Vallecios <00078217@uca.edu.sv>
2018-05-06 21:56:45 -0600

    Clase Habitacion y sus modificadores incluyendo los modificadores de piso

--- src/parcial/Habilitar_Habitacion.java
+++ src/parcial/Habilitar_Habitacion.java
@@ -6,62 +6,109 @@
 package parcial;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
- * @author Margarita
+ * @author DavidVallecios
  */
 public class Habilitar_Habitacion {
-     
    String Piso;
    int numero;
    boolean estado;
    private ArrayList<ArrayList<Habitacion>> pisoList;
    private ArrayList<Habitacion> habList;
+   List <String> codPiso;
+   String [] pisos = {"A","B","C","D","E","F"};
    Scanner lectura = new Scanner(System.in);
    
 
+   public Habilitar_Habitacion(){
+       List <String> codPiso = new ArrayList<String>();
+    }
    
-    public void agregarHabitacion(int indice){
+   
+    public Habitacion CrearHabitacion(int indice, String piso){
         Habitacion hab = new Habitacion();
         hab.setEstado(true);
         System.out.println("Ingrese el numero de la habitacion: ");
         hab.setNumero(indice);
-        switch(miniMenu()){
-            case 1:
-                hab.setTipo("Sencilla");
-                break;
-            case 2:
-                hab.setTipo("Doble");
-                break;
-            default:
-                System.out.println("Ingrese una opcion valida");
-                break;
+        hab.setPiso(piso);
+        if(indice%2 == 0){
+            hab.setTipo("Doble");
         }
-        habList.add(hab);
+        else{
+            hab.setTipo("Sencilla");
+        }
+        return hab;
     }
     
     public void agregarPiso(){
-        System.out.println("Cuantas habitaciones tendra el nuevo piso: MAXIMO 10");
-        int aux = lectura.nextInt();
-        if(aux > 10){
-            System.out.println("No se puede ingresar mas de 10");
+        int aux;
+        String pisoAux;
+        
+        while(true){
+            System.out.println("Ingrese el piso");
+            pisoAux = lectura.nextLine();
+            if(AnadiendoPisoArray(ValidarCodigoPiso(pisoAux))){
+                System.out.println("Intentelo de nuevo");
+            }
+            else{
+                break;
+            }
         }
-        else if(aux <= 0){
-            System.out.println("No se puede ingresar habitaciones con numero menor a 1");
+        
+        while(true){
+            System.out.println("Cuantas habitaciones tendra el nuevo piso: MAXIMO 10");
+            aux = lectura.nextInt();
+            if(aux > 10){
+                System.out.println("No se puede ingresar mas de 10");
+            }
+            else if(aux <= 0){
+                System.out.println("No se puede ingresar habitaciones con numero menor a 1");
+            }
+            else{
+                break;
+            }
         }
-        for(int i = 0; i < 10; i++){
-            
+        for(int i = 1; i < 11; i++){
+            habList.add(CrearHabitacion(i,pisoAux));
         }
-    }
- 
-    public int miniMenu(){
-        System.out.println("Ingrese el tipo de habitacion: ");
-        System.out.println("1. Sencilla");
-        System.out.println("2. Doble");
-        return lectura.nextInt();
+        pisoList.add(habList);
+        
     }
     
+    
+    public String ValidarCodigoPiso(String codigo_piso){
+        
+        while(true){
+                if(codigo_piso.length() > 1){
+                    System.out.println("El identificador del piso debe ser una letra.");
+                    System.out.println("Intente de nuevo");
+                }
+                else if(codigo_piso.isEmpty()){
+                    System.out.println("No ingrese datos vacios");
+                }
+                else{
+                    return codigo_piso;
+                }
+            }
+    }
+    
+    public boolean AnadiendoPisoArray(String piso){
+        codPiso = Arrays.asList(pisos);
+        boolean flag = false;
+        for(String item: codPiso){
+            if(item.equals(piso)){
+                System.out.println("Ese piso ya existe.");
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+ 
     public void habilitarHabitacion(String piso, int numero){
         
     
